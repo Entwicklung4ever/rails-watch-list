7e39d5e7e54f460b2df6e7eb57fc5de7316eddcf
Omar Eloumami <eloumami.omar1@gmail.com>
2024-01-19 15:01:25 +0100

    Watch list

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,8 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
-  validates :comment, length: { minimum: 6 }
-  validates :movie, presence: true
-  validates :list, presence: true
-  validates :movie, uniqueness: { scope: :list, message: "has already been bookmarked in this list" }
+
+  validates :comment, length: {minimum: 6}
+  validates :movie_id, uniqueness: {scope: :list_id, message: "is already in the list"}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/new'
-  get 'lists/create'
-  resources :lists, only: [:index, :show, :new, :create] do
-    resources :movies, only: [:index, :show, :new, :create, :destroy,]
-      resources :bookmarks, only: [:new, :create, :destroy]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "lists#index"
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
   end
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
